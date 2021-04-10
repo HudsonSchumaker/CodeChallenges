fc7ac5f3aa6ce37aae9c90f8fe1c75151aa5a04e
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-10 20:08:01 +0200

    add CdPlayer class.

--- Challenges/src/jukebox/CdPlayer.java
+++ Challenges/src/jukebox/CdPlayer.java
@@ -1,4 +1,40 @@
 package jukebox;
 
-public class CdPlayer {
+public class CdPlayer implements Player {
+
+    private CD cd;
+
+    private final Set<CD> cds;
+    private final Playlist playlist;
+
+    public CdPlayer(Playlist playlist, Set<CD> cds) {
+        this.playlist = playlist;
+        this.cds = cds;
+    }
+
+    protected void addCD(CD cd) {}
+    protected void removeCD(CD cd) {}
+
+    protected Playlist getPlaylist() {
+        return playlist;
+    }
+
+    @Override
+    public void playNextSong() {
+
+    }
+
+    @Override
+    public void playPrevSong() {
+
+    }
+
+    protected CD getCd() {
+        return cd;
+    }
+
+    protected Set<CD> getCds() {
+        return cds;
+    }
+
 }
