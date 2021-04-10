118601fd728fe7c1da8c6754a26f5698111b881f
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-10 20:13:08 +0200

    Add class

--- Challenges/src/jukebox/CD.java
+++ Challenges/src/jukebox/CD.java
@@ -1,4 +1,18 @@
 package jukebox;
 
+import java.util.List;
+
 public class CD {
+
+    private List<Song> songs;
+
+    // more details about CDs
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
 }
