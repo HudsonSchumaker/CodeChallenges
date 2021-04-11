c4c3326b2eb9c6a7d00279fd55a7d40d17976efa
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-11 15:06:18 +0200

    changes

--- challenges/src/jukebox/Song.java
+++ challenges/src/jukebox/Song.java
@@ -1,4 +1,22 @@
 package jukebox;
 
 public class Song {
+
+    private final String title;
+    private final CD cd;
+
+    // more song details
+
+    public Song(String title, CD cd) {
+        this.title = title;
+        this.cd = cd;
+    }
+
+    public CD getCd() {
+        return cd;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
