afad5aeebfc5d4ed06ea4f4b58fc00b9cec497ce
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-11 15:07:01 +0200

    changes

--- challenges/src/jukebox/Selector.java
+++ challenges/src/jukebox/Selector.java
@@ -1,4 +1,9 @@
 package jukebox;
 
-public class Selector {
+interface Selector {
+    public void nextSongBtn();
+    public void prevSongBtn();
+    public void addSongToPlaylistBtn(Song song);
+    public void removeSongFromPlaylistBtn(Song song);
+    public void shuffleBtn();
 }
